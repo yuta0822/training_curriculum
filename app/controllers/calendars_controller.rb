f061a1a020e2f876cff6b9d5ffd464324a2a3278
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-01-22 12:59:53 +0900

    Planのpを小文字に変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,14 +34,14 @@ class CalendarsController < ApplicationController
     7.times do |x|
       today_plans = []
       plans.each do |plan|
-        today_plans.push(Plan.plan) if plan.date == @todays_date + x
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       
       wday_num = Date.today.wday + x
       if wday_num >= 7
         wday_num = wday_num -7
       end
-      
+
       days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
